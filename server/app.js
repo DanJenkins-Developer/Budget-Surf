e412863a3d4a975ff9b80b4de391a6e4545ed34e
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-01-28 14:18:49 -0600

    Imported routes and error handlers

--- server/app.js
+++ server/app.js
@@ -8,10 +8,16 @@ const authenticateUser = require('./middleware/authentication');
 app.use(express.json());
 
 // router imports
-
+const authRouter = require('./routes/authRoute');
+// error handler imports
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 // routes
-
+app.use('/api/v1/auth', authRouter);
 // error handling middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 
 
 const PORT = 3000
