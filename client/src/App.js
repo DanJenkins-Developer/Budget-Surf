c847a0d8b2f2aa6697a6120d27f5fdcecd1fde7c
Atheer Auribi <83246461+atheerauribi@users.noreply.github.com>
2023-01-28 15:24:37 -0600

    added budget card component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import BudgetCard from './BudgetCard';
 
 function App() {
   return (
     <div className="App">
-        <h2>Hello World</h2>
+        <h2>Hello World!</h2>
     </div>
   );
 }
