79868decf0d577f94da0bb6e28c2e75688f4e9df
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-01-28 14:39:46 -0600

    Added basic pie chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
+import { PieChart } from 'react-minimal-pie-chart';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <h2>Hello World</h2>
+        <PieChart
+        data={[
+          //{ radius: 100 },
+          { title: 'One', value: 10, color: '#E38627' },
+          { title: 'Two', value: 15, color: '#C13C37' },
+          { title: 'Three', value: 20, color: '#6A2135' },
+        ]}
+/>
     </div>
+    
   );
 }
 
