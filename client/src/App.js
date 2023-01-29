8b4f6493026cd76e5f6ec36881283ab48bded42f
Atheer Auribi <83246461+atheerauribi@users.noreply.github.com>
2023-01-29 10:27:58 -0600

    fixed router import

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,13 @@ import axios from 'axios'
 import BudgetCard from './components/BudgetCard';
 import Login from './components/Login';
 import Register from './components/Register';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return ( 
         <div className="App">
           <Routes>
-            <Route path='/' element={<Login />}/>
+            <Route path="/" element={<Login />}/>
             <Route path='/register' element={<Register />}/>
             {/* <Route path='/budget' element={<BudgetCard name="Food" amount={201} max={400}/>}/> */}
             <Route path='/donut' element={<DonutChart
