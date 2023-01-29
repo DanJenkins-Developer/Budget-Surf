0ff2e146d63b78e966da5b192124b0ed91a0d25f
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-01-28 21:36:08 -0600

    Finished updateExpense functionality

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -26,7 +26,7 @@ const getExpense = async (req, res) => {
         createdBy: userId
     })
     if (!expense) {
-        throw new NotFoundError(`No job with id ${expenseId}`)
+        throw new NotFoundError(`No expense with id ${expenseId}`)
     }
     res.status(StatusCodes.OK).json({expense})
 }
@@ -36,7 +36,24 @@ const createExpense = async (req, res) => {
     res.status(StatusCodes.CREATED).json({expense})
 }
 const updateExpense = async (req, res) => {
-    res.send('update Expense')
+    const {
+        body: {amount},
+        user: {userId},
+        params: {id:expenseId}
+    } = req
+
+    if (amount === '') {
+        throw new BadRequestError('Amount field cannot be empty')
+    }
+    const expense = await Expense.findByIdAndUpdate(
+        {_id: expenseId, createdBy: userId},
+        req.body,
+        {new: true, runValidators: true}
+    )
+    if (!expense) {
+        throw new NotFoundError(`No expense with id ${expenseId}`)
+    }
+    res.status(StatusCodes.OK).json({expense})
 }
 const deleteExpense = async (req, res) => {
     const {
