4fd4d17dee9e81f3c5e2d309dcb7447ffed3bb85
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-01-29 01:07:11 -0600

    refactored app.js

--- server/app.js
+++ server/app.js
@@ -16,29 +16,28 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/expense', authenticateUser, expenseRouter)
-// error handling middleware
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
-
-
-const PORT = 5000
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>')
 })
 
+
+// error handling middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT
+
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI);
-    app.listen(PORT, () =>
-      console.log(`Server is listening on port ${PORT}...`)
-    );
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        })
     } catch (error) {
         console.log(error);
     }
 }
 
 start()
-
-
