3cea48e2b2ad51b98c0f172ffa720093ef97b780
Atheer Auribi <83246461+atheerauribi@users.noreply.github.com>
2023-01-28 23:11:16 -0600

    added functions to budget provider

--- client/src/contexts/BudgetContext.js
+++ client/src/contexts/BudgetContext.js
@@ -1,7 +1,40 @@
-export function useBudgets() {
+import React, { useContext, useState } from 'react'
 
+const BudgetContext = React.createContext()
+
+export function useBudgets() {
+    return useContext(BudgetContext)
 }
 
 export const BudgetProvider = ({ children }) => {
-    return children
+    const [budget, setBudget] = useState(0)
+    const [expenses, setExpenses] = useState([])
+
+    function getBudgetExpenses() {
+
+    }
+    function addExpense() {
+
+    }
+    function addBudget() {
+
+    }
+    function deleteBudget() {
+
+    }
+    function deleteExpense() {
+
+    }
+
+    return (
+        <BudgetContext.Provider value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteBudget,
+        deleteExpense,
+        }}> {children} </BudgetContext.Provider>
+    )
 }
