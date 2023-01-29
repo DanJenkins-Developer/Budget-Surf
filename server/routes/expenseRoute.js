af6477164b53636359b9f8fee7961a826803705d
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-01-28 23:19:10 -0600

    Imported and created showStats route

--- server/routes/expenseRoute.js
+++ server/routes/expenseRoute.js
@@ -7,11 +7,13 @@ const {
     getAllExpenses,
     updateExpense,
     getExpense,
+    showStats,
 
 } = require('../controllers/expenseController')
 
 router.route('/').post(createExpense).get(getAllExpenses)
 // stats/budget route tbd
+router.route('/stats').get(showStats)
 router.route('/:id').get(getExpense).delete(deleteExpense).patch(updateExpense)
 
 module.exports = router
